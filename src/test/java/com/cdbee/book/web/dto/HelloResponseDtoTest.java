ff113bdf7bcd7a3b2ccb19f5cff0eeb229f83c0d
choedobby <frannacdh@gmail.com>
2021-11-30 13:16:24 +0900

    Chapter3 Simple save, update, JPA Auditing and their tests

---
+++ src/test/java/com/cdbee/book/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.cdbee.book.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class HelloResponseDtoTest {
+    @Test
+    public void lombok_test(){
+        //given
+        String name = "Test";
+        int amount = 10000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
